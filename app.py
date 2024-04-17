9e85abe45c2da62e7adf2e5dac695381f069cb87
Kav1n-Lal <116146011+Kav1n-Lal@users.noreply.github.com>
2024-04-17 16:12:28 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ def add_vertical_space(spaces=1):
     for _ in range(spaces):
         st.sidebar.markdown("---")
 
-
 def main():
     
     st.set_page_config(page_title="Llama-2-GGML Desi Rail Chatbot")
@@ -30,32 +29,26 @@ def main():
         - **GitHub:** [ThisIs-Developer/Llama-2-GGML-CSV-Chatbot](https://github.com/ThisIs-Developer/Llama-2-GGML-CSV-Chatbot) 💬
     ''')
 
-   
-    
-    
-
     def processing():
-        # specify the location of the d2.csv file below
-        uploaded_file=**'C:\\Users\\New folder\\d2.csv'**
-        
         # vectorestore folder path
         DB_FAISS_PATH = "vectorstore/db_faiss"
         add_vertical_space(1)
         
         st.sidebar.write('Made by [@ThisIs-Developer](https://huggingface.co/ThisIs-Developer)')
+        
+        # specify the location of the chat.csv file below
+        # copy the cloned repository path and at the end add "\\chat.csv"
+        uploaded_file='C:\\Users\\Kavin\\Desktop\\desi_rail_chatbot\\chat.csv' 
 
         if uploaded_file is not None:
-        
-            file_path=**'C:\\Users\\New folder\\d2.csv'**
-        
+            
+            file_path=uploaded_file
             st.write("Processing available information...")
         
-
-
             loader = CSVLoader(file_path=file_path, encoding="utf-8", csv_args={'delimiter': ','})
             data = loader.load()
 
-            text_splitter = RecursiveCharacterTextSplitter(chunk_size=300, chunk_overlap=20)#500,20
+            text_splitter = RecursiveCharacterTextSplitter(chunk_size=300, chunk_overlap=20)
             text_chunks = text_splitter.split_documents(data)
 
             st.write(f"Total text chunks: {len(text_chunks)}")
@@ -69,8 +62,6 @@ def main():
                                 max_new_tokens=512,
                                 temperature=0.2)
             
-
-
             qa = ConversationalRetrievalChain.from_llm(llm, retriever=docsearch.as_retriever())
 
             st.write("Enter your query:")
@@ -82,16 +73,13 @@ def main():
                     result = qa({"question": prompt, "chat_history": chat_history})
                     st.write("Response:", result['answer'])
                    
-                    
             os.remove(file_path)
             
     def initialize(x,y):
         
         with st.form(key='my_form'):
-            
-            
             df1=df[(df['Source Station Name'] == x) & (df['Destination Station Name'] == y)]
-            df1[['Train No', 'Train Name', 'Sequences','Station Name', 'Next Station','Arrival time', 'Departure Time', 'Distance1']].to_csv('d2.csv',encoding='utf-8')
+            df1[['Train No', 'Train Name', 'Sequences','Station Name', 'Next Station','Arrival time', 'Departure Time', 'Distance1']].to_csv('chat.csv',encoding='utf-8')
             submit_button = st.form_submit_button(label='Chat')
 
             if submit_button:
